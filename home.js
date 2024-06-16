bd253ee0bcc95cbf0c116b04034f11eec6365204
Kellbie <93326149+Kellbie@users.noreply.github.com>
2024-06-16 21:33:28 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -13,21 +13,6 @@ function startAutoScroll() {
   }
 }
 
-let isInteracted = false;
-
-function startAutoScroll() {
-  if (!isInteracted) {
-    isInteracted = true;
-    setInterval(() => {
-      const carousel = document.querySelector(".carousel");
-      const firstItem = carousel.firstElementChild;
-      carousel.style.transition = "transform 0.5s ease";
-      carousel.style.transform = "translateX(-100%)";
-      carousel.appendChild(firstItem);
-    }, 2000); // Adjust the time interval as needed
-  }
-}
-
 const toggleButton = document.getElementById('toggleButton');
   const underLastDiv = document.querySelector('.under-last');
 
