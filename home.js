f522dd5165685897783c2ba91dc85723a3ac60ff
Kellbie <93326149+Kellbie@users.noreply.github.com>
2024-06-18 10:48:02 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -24,3 +24,33 @@ const toggleButton = document.getElementById('toggleButton');
     // Toggle display of the div with class 'under-last'
     underLastDiv.style.display = 'block';
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('wildlife-btn');
+    const span = document.querySelector('.wildlife-text');
+  
+    button.addEventListener('click', function() {
+      button.style.display = 'none';
+      span.style.display = 'block';
+    });
+  });
+
+
+
+
+  document.getElementById('wildlife-btn-less').addEventListener('click', function() {
+    var wildlifeText = document.querySelector('.wildlife-text');
+    wildlifeText.style.display = 'none';
+
+    var displayBtn = document.getElementById('wildlife-btn');
+    displayBtn.style.display = 'block';
+  });
+
+
+  document.getElementById('comfort-btn').addEventListener('click', function() {
+    var comfortText = document.querySelector('.comfort-text');
+    comfortText.style.display = 'block';
+
+    var comfortBtn = document.getElementById('comfort-btn');
+    comfortBtn.style.display = 'none';
+  });
